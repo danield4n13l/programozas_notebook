56a31b6fbca95f865b24325f6040a0a7c651a667
danield4n13l <danipupak2@gmail.com>
2022-08-27 16:32:03 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+
+namespace Program
+{
+    /// <summary>
+    /// Egy példa a struktúrák használatára.
+    /// </summary>
+    /// <see langword="struct"/>
+    struct Könyv
+    {
+        public string név;
+        public int szám;
+        public void set(string név, int szám = -1)
+    {
+        this.név = név;
+        this.szám = szám;
+    }
+    }
+    class Program
+    {
+        public static int Négyzet(int x) => x * x;
+
+
+
+        /// <summary>
+        /// Köszönt.
+        /// </summary>
+        /// <param name="név">A név akit köszönt</param>
+        /// <example>
+        /// Egy példa erre:
+        /// <code>
+        ///     Szia("Péter");
+        /// </code>
+        /// Kimenet:    <c>Szia Péter!</c>
+        /// </example>
+        static void Szia(string név) => Console.WriteLine($"Szia ${név}!");
+        static void Main(string[] args)
+        {
+
+            string asd = "nuiznf";
+            string qwi = new String(new char[] {'h', 'e', 'l', 'l', 'o', '\x00'});
+
+
+        }
+    }
+}
